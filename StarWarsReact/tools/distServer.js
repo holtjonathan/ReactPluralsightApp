80289f3a8044dd529102c0e83958590a194de903
holtjonathan <holtjonathan@gmail.com>
2019-01-22 00:43:52 -0500

    All done!

--- StarWarsReact/tools/distServer.js
+++ StarWarsReact/tools/distServer.js
@@ -2,12 +2,14 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import compression from 'compression';
 
 /*eslint-disable no-console*/
 
-const port = 3000;
+const port = 3001;
 const app = express();
 
+app.use(compression());
 app.use(express.static('dist'));
 
 app.get('*', function(req, res) {
--- StarWarsReact/webpack.config.prod.js
+++ StarWarsReact/webpack.config.prod.js
@@ -3,13 +3,14 @@ import path from 'path';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 const GLOBALS = {
-  'process.env.NODE_ENV': JSON.stringify('production')
+  'process.env.NODE_ENV': JSON.stringify('production'), //This global makes sure React is built in prod mode. https://facebook.github.io/react/downloads.html
+  __DEV__: false // potentially useful for feature flags. More info: https://github.com/petehunt/webpack-howto#6-feature-flags
 };
 
 export default {
   debug: true,
-  devtool: 'source-map',
-  noInfo: false,
+  devtool: 'source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
+  noInfo: true, // set to false to see a list of every file being bundled.
   entry: './src/index',
   target: 'web',
   output: {
@@ -21,7 +22,7 @@ export default {
     contentBase: './dist'
   },
   plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin(GLOBALS),
     new ExtractTextPlugin('styles.css'),
     new webpack.optimize.DedupePlugin(),
@@ -30,11 +31,11 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
-      {test: /(\.css)$/, loaders: ExtractTextPlugin.extract("css?sourceMap")},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+      {test: /(\.css)$/, loader: ExtractTextPlugin.extract("css?sourceMap")},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
+      {test: /\.(woff|woff2)$/, loader: "url?prefix=font/&limit=5000"},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"}
     ]
   }
 };
